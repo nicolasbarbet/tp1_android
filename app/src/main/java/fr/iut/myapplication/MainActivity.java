cd5b3f0339a83df25a11ad269bd835eff568ca2a
Nicolas Barbet <21800428@etu.unicaen.fr>
2020-01-20 11:57:41 +0100

    Prénom OK

--- app/src/main/java/fr/iut/myapplication/MainActivity.java
+++ app/src/main/java/fr/iut/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,4 +18,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
 }
--- app/src/main/java/fr/iut/myapplication/prenom.java
+++ app/src/main/java/fr/iut/myapplication/prenom.java
@@ -3,12 +3,27 @@ package fr.iut.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class prenom extends AppCompatActivity {
+
+    private EditText input;
+    private TextView tvPrenom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prenom);
+        input = findViewById(R.id.editText);
+        tvPrenom = findViewById(R.id.textView2);
+    }
+
+
+    public void onClickValider(View view){
+        String prenom = "Bonjour " + input.getText().toString() + " !";
+        tvPrenom.setText(prenom);
+        tvPrenom.setVisibility(View.VISIBLE);
     }
 }
